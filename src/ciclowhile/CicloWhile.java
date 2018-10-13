a9e45e9f50271b214c821a64d7d60a1fab9cfbc0
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:36:19 -0600

    Initial commit

---
+++ src/ciclowhile/CicloWhile.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclowhile;
+
+/**
+ *
+ * @author Yova
+ */
+public class CicloWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int valor = 0;
+        while (valor < 10) {            
+            System.out.println(valor);
+            valor +=2;
+        }
+    }
+    
+}
